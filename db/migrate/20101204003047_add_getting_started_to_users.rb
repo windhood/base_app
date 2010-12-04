bb0c9eac1188a957c5193489f560c317d406bac8
Windhood <guanwh@gmail.com>
2010-12-03 17:17:19 -0800

    add getting_started flag to user

---
+++ db/migrate/20101204003047_add_getting_started_to_users.rb
@@ -0,0 +1,9 @@
+class AddGettingStartedToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :getting_started, :bool
+  end
+
+  def self.down
+    remove_column :users, :getting_started
+  end
+end
