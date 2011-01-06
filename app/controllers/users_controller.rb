4da2ed788de419a09f9f913908e5009da82e7525
Windhood <guanwh@gmail.com>
2011-01-05 17:24:17 -0800

    add controller function to update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,21 @@ class UsersController  < ApplicationController
       redirect_to getting_started_path
     end
   end
+  
+  def update
+    params[:user] ||= {}
+    params[:user][:searchable] ||= false
+    
+    if current_user.update params[:user]
+      flash[:notice] = I18n.t 'people.update.updated'
+    else
+      flash[:error] = I18n.t 'people.update.failed'
+    end
+    
+    if params[:getting_started]
+      redirect_to getting_started_path(:step => params[:getting_started].to_i+1)
+    else
+      redirect_to edit_user_path
+    end
+  end
 end
