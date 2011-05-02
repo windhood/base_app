9847353c8676f67aecfb3d7f7d9140463affd393
Windhood <guanwh@gmail.com>
2011-05-02 16:46:57 -0700

    add theme browser

--- app/controllers/wowos_controller.rb
+++ app/controllers/wowos_controller.rb
@@ -1,12 +1,19 @@
 class WowosController  < ApplicationController
   def index
   end
+  
   def new
     @themes = Theme.all
     new_wowo_with_guid #so that we could generate preview url
+    unless @themes.blank?
+      @wowo.theme = @themes[0]
+    end
   end
+  
   def preview
   end
+  
+  ################### Private functions ############################
   private
   def new_wowo_with_guid
     @wowo = Wowo.new
